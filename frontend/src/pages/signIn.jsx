841ab9263ab375fd7a7c255fe936fdc65ed26d56
tronzard <satyamkhatiwada1999@gmail.com>
2026-03-03 14:53:21 +0545

    added new signin page

--- frontend/src/pages/signIn.jsx
+++ frontend/src/pages/signIn.jsx
@@ -2,74 +2,57 @@ import React from "react";
 
 export default function SignIn() {
   return (
- <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className="flex justify-center items-center min-h-screen bg-base-100">
+      
+      <div className="flex bg-base-200 rounded-xl shadow-lg overflow-hidden max-w-4xl">
+
+        <div className="w-1/2 bg-white flex items-center justify-center p-8">
           <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-            className="mx-auto h-10 w-auto"
+            src="src\assets\signInIllustration.png"
+            alt="shopping illustration"
+            className="max-h-72"
           />
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
-            <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                />
-              </div>
-            </div>
+        <div className="w-1/2 p-10 flex flex-col justify-center">
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                />
-              </div>
-            </div>
+          <h2 className="text-xl font-semibold mb-6">
+            Please Log In To Your Account
+          </h2>
 
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
+          <input
+            type="email"
+            placeholder="Email"
+            className="input input-bordered w-full mb-4"
+          />
 
-          <p className="mt-10 text-center text-sm/6 text-gray-400">
-            Not a member?{' '}
-            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
-              Start a 14 day free trial
-            </a>
-          </p>
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered w-full mb-4"
+          />
+
+          <div className="text-sm mb-6">
+            <label className="flex items-center gap-2">
+              <input type="checkbox" className="toggle toggle-sm" />
+              Remember Me
+            </label>
+
+            <a className="text-red-500 block w-full text-left pt-4">Forgot Password?</a>
+          </div>
+
+          <div className="flex gap-4">
+            <button className="btn flex-1">Login</button>
+          </div>
+
+          <div className="flex gap-4 mt-2">
+            <button className="btn flex-1 bg-gray-500!">Sign Up</button>
+          </div>
+
         </div>
+
       </div>
+
+    </div>
   );
 }
